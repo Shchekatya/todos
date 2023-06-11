13246b0f607ecf54a2c9379231e603ed88e11069
Shchekatya <shchekatya@gmail.com>
2023-06-12 00:29:52 +0300

    added some style

--- src/components/add-to-do/addToDo.js
+++ src/components/add-to-do/addToDo.js
@@ -1,21 +1,20 @@
-import {    TextField} from "@mui/material";
-import {
-    NavLink,
-    Outlet
-} from 'react-router-dom';
+import {TextField} from "@mui/material";
+import {NavLink, Outlet} from 'react-router-dom';
+import './addToDo.scss'
 
 const AddToDo = () => {
     return ( 
-     <>
+     <div className="todo-wrap">
      <header>
-     <TextField id = "outlined-basic"
-        label = "Outlined"
-        variant = "outlined" />
-     </header>        
+        <h1 className="todo-name">todos</h1>
+        <TextField id = "outlined-basic"
+          label = "Outlined"
+          variant = "outlined" />
+        </header>        
         <main >
         < Outlet />
         </main> 
-     </>
+     </div>
     )
 }
 
